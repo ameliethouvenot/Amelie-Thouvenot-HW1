11af1708478cab257a97649b887a1111b0dfb56a
Amelie Thouvenot <amelie.thouvenot@kellogg.northwestern.edu>
2024-01-23 02:50:20 +0000

    Changes4

--- kmdbamelie.sql
+++ kmdbamelie.sql
@@ -5,63 +5,76 @@ DROP TABLE IF EXISTS actors;
 .mode column
 .headers off
 
--- any more tables?
-
 CREATE TABLE studios (
     id INTEGER PRIMARY KEY AUTOINCREMENT, 
-    name TEXT
+    name_studios TEXT
 );
 
 CREATE TABLE movies (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    studios_id INTEGER REFERENCES studios(id), 
+    studios_id INTEGER, 
     movie_title TEXT,
     year_released TEXT, 
     MPAA_rating TEXT, 
-    character TEXT
+    --character_id INTEGER
 );
 
 CREATE TABLE actors (
     id INTEGER PRIMARY KEY AUTOINCREMENT, 
     name_actor TEXT,
-    character TEXT
+    characters_id INTEGER
 );
 
--- actor_id INTEGER into movies table? Many of many relationship
--- include studios_name into table movies?
--- going to need an interjoin function between movies & actors 
+CREATE TABLE characters (
+    id INTEGER PRIMARY KEY AUTOINCREMENT, 
+    name_character TEXT, 
+    movies_id INTEGER
+)
 
 INSERT INTO studios (
-    id, 
-    name 
+    name_studios 
 ) VALUES 
-    ("1", "Warner Bros."),
-    ("2", "Paramount"), 
-    ("3", "Universal"), 
-    ("4", "Disney"); 
+    ("Warner Bros."),
+    ("Disney"); 
+
+INSERT INTO actors (
+    name_actor, 
+    characters_id
+) VALUES 
+    ("Christian Bale", 1),
+    ("Michael Caine", 2), 
+    ("Liam Neeson", 3), 
+    ("Katie Holmers", 4),
+    ("Gary Oldman", 5),
+    ("Heath Ledger", 6), 
+    ("Aaron Eckhart", 7), 
+    ("Maggie Gyllenhaal", 4),
+    ("Tom Hardy", 8), 
+    ("Joseph Gordon-Levitt", 9), 
+    ("Anne Hathaway", 10); 
 
 --need to take into account character
 INSERT INTO movies (
-    id, 
     studios_id,
     movie_title,
     year_released, 
     MPAA_rating 
 )
  VALUES 
-    (1, "?", "Batman Begins", "2005", "PG-13"), 
-    (2, "?", "The Dark Knight", "2008", "PG-13"), 
-    (3, "?", "The Dark Knight Rises", "2012", "PG-13");
+    (1, "Batman Begins", "2005", "PG-13"), 
+    (1, "The Dark Knight", "2008", "PG-13"), 
+    (1, "The Dark Knight Rises", "2012", "PG-13");
 
 
 -- SELECT STATEMENTS TO PROVIDE ADEQUATE OUTPUT
 -- Movie list output --> anything else needed to have studios_name printed, 
 -- or the studio_id reads automatically??
 
---SELECT movie_title, year_released, MPAA_rating, studios_name
---FROM movies 
---GROUP BY year_released 
---ORDER BY year_released; 
+SELECT movie_title, year_released, MPAA_rating, studios_name
+FROM movies 
+INNER JOIN studios ON studios_id = movies.studio_id 
+GROUP BY year_released 
+ORDER BY year_released; 
 
 --SELECT movies.movie_title, actors.name_actor, actors.character
 --FROM actors INNER JOIN character ON movies.character = actors.character
