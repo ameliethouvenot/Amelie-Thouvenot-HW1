c922078a3147370efe0b770930fab27b594e527a
Amelie Thouvenot <amelie.thouvenot@kellogg.northwestern.edu>
2024-01-23 04:17:15 +0000

    changes9

--- kmdbamelie.sql
+++ kmdbamelie.sql
@@ -19,16 +19,17 @@ CREATE TABLE movies (
     MPAA_rating TEXT 
 );
 
-CREATE TABLE actors (
-    id INTEGER PRIMARY KEY AUTOINCREMENT, 
-    name_actor TEXT,
-    characters_id INTEGER
-);
 
 CREATE TABLE characters (
     id INTEGER PRIMARY KEY AUTOINCREMENT, 
     name_character TEXT, 
     movies_id INTEGER
+);
+
+CREATE TABLE actors (
+    id INTEGER PRIMARY KEY AUTOINCREMENT, 
+    name_actor TEXT,
+    characters_id INTEGER
 );
 
 INSERT INTO studios (
@@ -48,6 +49,25 @@ INSERT INTO movies (
     (1, "The Dark Knight", "2008", "PG-13"), 
     (1, "The Dark Knight Rises", "2012", "PG-13");
 
+INSERT INTO characters (
+    name_character, movies_id
+) VALUES
+    ("Bruce Wayne", 1),
+    ("Bruce Wayne", 2),
+    ("Bruce Wayne", 3),
+    ("Alfred", 1),
+    ("Alfred", 2),  
+    ("Ra's Al Ghul", 1),
+    ("Rachel Dawes", 1),
+    ("Rachel Dawes", 2),
+    ("Commissioner Gordon", 1),
+    ("Commissioner Gordon", 3),
+    ("Joker", 2),
+    ("Harvey Dent", 2), 
+    ("Bane", 3), 
+    ("John Blake", 3), 
+    ("Selina Kyle", 3); 
+
 INSERT INTO actors (
     name_actor, 
     characters_id
@@ -64,23 +84,7 @@ INSERT INTO actors (
     ("Joseph Gordon-Levitt", 13), 
     ("Anne Hathaway", 14); 
 
-INSERT INTO characters (
-    name_character, movies_id
-) VALUES
-    ("Bruce Wayne", 1),
-    ("Bruce Wayne", 2),
-    ("Bruce Wayne", 3),
-    ("Alfred", 1),
-    ("Alfred", 2),  
-    ("Ra's Al Ghul", 1),
-    ("Rachel Dawes", 1),
-    ("Commissioner Gordon", 1),
-    ("Commissioner Gordon", 3),
-    ("Joker", 2),
-    ("Harvey Dent", 2), 
-    ("Bane", 3), 
-    ("John Blake", 3), 
-    ("Selina Kyle", 3); 
+
 
 SELECT movie_title, year_released, MPAA_rating, name_studios
 FROM movies 
@@ -88,12 +92,11 @@ INNER JOIN studios ON studios_id = movies.studios_id
 GROUP BY year_released 
 ORDER BY year_released; 
 
-
-SELECT movies.movie_title, actors.name_actor, characters.name_character
+SELECT movies.movie_title,  characters.name_character
 FROM characters 
 INNER JOIN movies ON movies.id = characters.movies_id
-INNER JOIN actors ON actors.characters_id = characters_id 
-GROUP BY movie_title;   
+--INNER JOIN actors ON actors.characters_id = characters_id;   
 
 
 
+--actors.name_actor,
